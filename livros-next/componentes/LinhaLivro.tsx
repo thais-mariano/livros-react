ca523a353c6999980386bb1401b6c843cd9e4470
thais-mariano <thaismariano89@gmail.com>
2024-10-23 22:20:44 -0300

    feat: add logic to livros-next

--- livros-next/componentes/LinhaLivro.tsx
+++ livros-next/componentes/LinhaLivro.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import ControleEditora from '../classes/controle/ControleEditora';
+import { Livro } from '../classes/modelo/Livro'; 
+
+const controleEditora = new ControleEditora();
 
 interface LinhaLivroProps {
-  titulo: string;
-  autores: string[];
-  editora: string;
+  livro: Livro;
+  excluir: (codigo: number) => void;
 }
 
-const LinhaLivro: React.FC<LinhaLivroProps> = ({ titulo, autores, editora }) => {
+const LinhaLivro: React.FC<LinhaLivroProps> = (props) => {
+  const { livro, excluir } = props;
+  const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
+
   return (
     <tr>
-      <td>{titulo}</td>
-      <td>{autores.join(', ')}</td>
-      <td>{editora}</td>
+      <td>{livro.titulo}</td>
+      <td>{livro.resumo}</td>
+      <td>{nomeEditora}</td>
+      <td>{livro.autores.join(', ')}</td>
+      <td>
+        <button onClick={() => excluir(livro.codigo)}>Excluir</button>
+      </td>
     </tr>
   );
 };
